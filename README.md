80cc1550cd72e8fea02b87fbd71fca2f55c8613c
reyan1357 <singhhimangshu20@gmail.com>
2023-04-17 19:52:20 +0530

    scoop batch installer

---
+++ README.md
@@ -0,0 +1,6 @@
+## Simple script to batch install software from scoop archive.
+
+### How to use:
+
+1. Add your package names to install_list.txt line-by-line.
+2. Run **_scoop.bat_** as admin and select your action.
---
+++ scoop.bat
@@ -0,0 +1,74 @@
+@echo OFF
+
+NET SESSION >nul 2>&1
+
+IF %ERRORLEVEL% EQU 0 (
+
+   ECHO.
+
+) ELSE (
+
+   ECHO.-----------------------------------------------------------------
+
+   ECHO OPPS, NOT RUNNING AS ADMIN !!!
+
+   ECHO.-----------------------------------------------------------------
+
+   PAUSE
+
+   EXIT /B 1
+
+)
+
+CLS
+
+ECHO ..........................................
+ECHO "batch installer for scoop"
+
+:MENU
+ECHO ...........................................
+ECHO SELECT ACTION:
+ECHO ...........................................
+ECHO.
+ECHO 1 - install app(s)
+ECHO 2 - update app(s)
+ECHO 3 - exit
+ECHO ...........................................
+
+
+SET /P M=Type 1, 2 or 3 then press ENTER:
+IF %M%==1 GOTO INS
+IF %M%==2 GOTO UPG
+IF %M%==3 GOTO EXT
+
+:INS
+setlocal EnableDelayedExpansion
+
+SET i=0
+FOR /F %%a IN (%~dp0\packages.txt) do (
+   SET /A i+=1
+   SET array[!i!]=%%a
+)
+SET n=%i%
+
+FOR /L %%i IN (1,1,%n%) do scoop install !array[%%i]!
+
+ECHO .....................
+ECHO Installation is done.
+ECHO .....................
+PAUSE
+CLS
+GOTO MENU
+
+:UPG
+scoop update all
+ECHO ..................
+ECHO Updating finished.
+ECHO ..................
+PAUSE
+CLS
+GOTO MENU
+
+
+:EXT
+EXIT
